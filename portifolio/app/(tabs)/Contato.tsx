d978e63840d4f6cc935f46a60da07cfe6f14d523
HenriqueFrancaa <henrique23.franca@gmail.com>
2025-05-25 21:17:27 -0300

    Finalizando Link pra contato!

--- portifolio/app/(tabs)/Contato.tsx
+++ portifolio/app/(tabs)/Contato.tsx
@@ -1,4 +1,4 @@
-import {View, Text, StyleSheet, Image} from 'react-native';
+import {View, Text, StyleSheet, Image, TouchableOpacity, Linking} from 'react-native';
 import FooterNav from '../components/footerbar';
 import Header from '../components/header';
 
@@ -9,15 +9,19 @@ export default function Contato(){
             <View>
                 <Text style={styles.title}>Contato</Text>
                 <Text style={styles.text}>
-                    Para entrar em contato, envie um e-mail para:
+                    Aqui estão algumas formas de entrar em contato comigo. Você pode me encontrar nessas redes sociais.
                 </Text>
                 <View style={styles.contact}>
-
-                <Image source={require("../../assets/images/linkedin.png")} style={styles.image}/>
-                <Image source={require("../../assets/images/github.png")} style={styles.image}/>
-                <Image source={require("../../assets/images/instagram.png")} style={styles.image}/>
+                    <TouchableOpacity onPress={() => Linking.openURL('https://www.linkedin.com/in/henrique-franca-16390125b/')}>
+                        <Image source={require("../../assets/images/linkedin.png")} style={styles.image}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => Linking.openURL('https://github.com/HenriqueFrancaa')}>
+                        <Image source={require("../../assets/images/github.png")} style={styles.image}/>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => Linking.openURL('https://www.instagram.com/henriq.francaa/')}>
+                        <Image source={require("../../assets/images/instagram.png")} style={styles.image}/>
+                    </TouchableOpacity>
                 </View>
-    
             </View>
             <FooterNav/>
         </View>
@@ -29,10 +33,9 @@ const styles = StyleSheet.create({
          backgroundColor: "#070743",
         flex: 1,
         flexDirection: "column",
-        paddingTop: 40, // Reduzido para aproximar do topo
+        paddingTop: 40,
         paddingBottom: 60,
         justifyContent: "center",
-        alignItems: "center",
         padding: 3
     },
     title: {
@@ -40,26 +43,30 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: '#eaf6ff',
         marginBottom: 10,
+        textAlign: 'center',
     },
     text: {
-        fontSize: 16,
+        fontSize: 14,
         color: '#eaf6ff',
         textAlign: 'center',
         marginBottom: 20,
         paddingHorizontal: 10,
+        width: '100%',
+        fontWeight: 'bold',
     },
     image: {
         width: 100,
         height: 100,
         borderRadius: 8,
-        marginBottom: 3,
+        marginBottom: 20,
     },
     contact :{
         flexDirection: 'column',
-        justifyContent: 'space-around',
+        justifyContent: 'center',
+        alignItems: 'center',
         width: '100%',
         padding: 10,
-        paddingHorizontal: 20,
+        paddingVertical: 20,
         marginTop: 20,
     }
-})
+});
